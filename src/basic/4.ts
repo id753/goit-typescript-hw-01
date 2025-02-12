1a74c4b0d785f7f4ed9fd8e0ac7527df92d54218
Evgen <mimywhy@gmail.com>
2025-02-12 17:27:32 +0200

    All hw

---
+++ src/basic/4.ts
@@ -0,0 +1,13 @@
+function showMessage(message): void {
+  console.log(message);
+}
+
+function calc(num1, num2): number {
+  return num1 + num2;
+}
+
+function customError(): string {
+  throw new Error("Error");
+}
+
+// Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
---
+++ src/generics/3.ts
@@ -0,0 +1,5 @@
+// У вас є функція merge, яка поєднує два об'єкти. Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
+
+function merge<T, K>(objA: T, objB: K): T & K {
+  return Object.assign(objA, objB);
+}
